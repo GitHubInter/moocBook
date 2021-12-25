9a07efd4cf93105bc6f2ac3b3c63d7fa909d023a
FuCong <githubinter@outlook.com>
2021-12-25 21:51:59 +0800

    创建book对象

--- router/book.js
+++ router/book.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const multer = require('multer')// 上传插件
 const Result = require('../models/result')
+const Book = require('../models/book')
 const { UPLOAD_PATH } = require('../utils/constant')
 
 const router = express.Router()
@@ -12,6 +13,8 @@ router.post(
     if (!req.file || req.file.length === 0) {
       new Result('上传电子书失败').fail(res)
     } else {
+      const book = new Book(req.file)
+      console.log(book)
       new Result('上传电子书成功').success(res)
     }
   }
--- utils/constant.js
+++ utils/constant.js
@@ -1,5 +1,6 @@
 const { env } = require('./env')
 const UPLOAD_PATH = env === 'dev' ? 'E:/work/2021year-end/test/upload/admin-upload-ebook' : '/root/upload/admin-upload/ebook';
+const UPLOAD_URL = env === 'dev' ? 'http://yoyu.online:8089/admin-upload-ebook' : 'https://yoyu.online:8089/admin-upload-ebook';
 
 module.exports = {
   CODE_ERROR: -1,
@@ -9,5 +10,7 @@ module.exports = {
   PWD_SALT: 'admin_imooc_node',
   PRIVATE_KEY: 'admin_imooc_node_test_youbaobao_xyz',
   JWT_EXPIRED: 60 * 60, // token失效时间
-  UPLOAD_PATH
+  UPLOAD_PATH,
+  UPLOAD_URL,
+  MIME_TYPE_EPUB: 'application/epub' // 老师那里解析结果为application/epub+zip
 }
